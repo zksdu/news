b3f9cd268f5620610ce9774dae9c134a7a941d64
zksdu <zksdu@qq.com>
2017-05-18 08:32:22 +0800

    修改文本内容无法改变的bug

--- News/app/src/main/java/cn/gdcp/news/NewsFragment.java
+++ News/app/src/main/java/cn/gdcp/news/NewsFragment.java
@@ -4,6 +4,7 @@ package cn.gdcp.news;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,19 +16,21 @@ import android.widget.TextView;
 
 public class NewsFragment extends Fragment {
     private TextView txtContent;
+    private String text;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        Log.e("AAA", "onCreateView");
         View view = inflater.inflate(R.layout.fragment_news, container, false);
 
         txtContent = (TextView) view.findViewById(R.id.txt_content);
+        txtContent.setText(text);
         return view;
     }
 
     public void setTxtContent(String text){
-        if(txtContent != null){
-            txtContent.setText(text);
-        }
+        Log.e("AAA", "setTxtContent");
+        this.text = text;
     }
 }
